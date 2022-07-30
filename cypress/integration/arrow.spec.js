fb5b3873c989857b6aa5b14d69faf645ac32e765
luccasgois <luccasgois1@gmail.com>
2022-07-30 13:20:37 -0300

    Arrow functions aula

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,19 @@
+it('Formas de utilizar arrow functions', function() {})
+
+function soma(a,b) {
+    return a + b;
+}
+
+// const soma = function (a, b){
+//     return a+b
+// }
+
+// const soma = (a, b) => {
+//     return a+b
+// }
+
+// const soma = (a, b) => a + b
+
+// const soma = (a, b) => a + b;
+
+console.log(soma(1,4))
