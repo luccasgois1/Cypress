3fe59c031fe3024abf751ebb9a801d35d20d419d
luccasgois <luccasgois1@gmail.com>
2022-08-02 23:27:55 -0300

    Armazenando variaveis apra usos futuros

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Cypress basics", () => {
-    it.only("Should visit a page and assert the title", () => {
+    it("Should visit a page and assert the title", () => {
         
         // Remova o comentario abaixo se quiser debugar o teste
         // cy.pause() // Usado para debugar o teste
