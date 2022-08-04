bf1d4e01a2694eaf63e64e2cb470fcad1741c03c
luccascleann <luccas@cleanndistribuidora.com>
2022-08-04 09:56:35 -0300

    Add testcases alert.spec.js

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -26,4 +26,40 @@ describe('Alerts...', () => {
         })
 
     });
+
+    it('Confirm message', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    });
+
+    it('Deny message', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+    });
+
+    it('Prompt', () => {
+        const expected_typed_value = '42'
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns(expected_typed_value)
+        })
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal(`Era ${expected_typed_value}?`)
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+    });
+
 });
