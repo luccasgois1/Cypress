38a5b2a61b7429ffa0ebab10b826ee337a761e8f
luccascleann <luccas@cleanndistribuidora.com>
2022-08-04 09:30:56 -0300

    Add testcase alert with mock

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -10,10 +10,20 @@ describe('Alerts...', () => {
         cy.reload()
     })
     
-    it('Checking alert message', () => {
+    it('Alert message', () => {
         cy.get('#alert').click()
         cy.on('window:alert', msg => {
             expect(msg).to.be.equal('Alert Simples')
         })
+    });
+    it('Alert message with Mock', () => {
+        const expected_alert_message = 'Alert Simples'
+        const stub = cy.stub().as('alert')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith(expected_alert_message)
+        })
+
     });
 });
